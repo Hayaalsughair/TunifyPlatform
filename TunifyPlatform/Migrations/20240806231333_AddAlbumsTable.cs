638f59503eb49d8dc7152be2534b829f05721311
Hayaalsughair <111651652+Hayaalsughair@users.noreply.github.com>
2024-08-07 03:03:31 +0300

    Add Models/Migrations/DbContext/Seeds

---
+++ TunifyPlatform/Migrations/20240806231333_AddAlbumsTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TunifyPlatform.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAlbumsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "albums",
+                columns: table => new
+                {
+                    AlbumsId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Album_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Release_Date = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Artist_Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_albums", x => x.AlbumsId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "albums");
+        }
+    }
+}
---
+++ TunifyPlatform/Models/Playlists.cs
@@ -0,0 +1,10 @@
+﻿namespace TunifyPlatform.Models
+{
+    public class Playlists
+    {
+        public int PlaylistsId { get; set; }
+        public int User_Id { get; set; }//FK
+        public string Playlists_Name { get; set; }
+        public string Created_Date { get; set; }
+    }
+}
